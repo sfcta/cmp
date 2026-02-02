9800f1dc775c603b5a032fa12ac53fe9c300f697
CHOW Chun Ho <chunhochow@users.noreply.github.com>
2026-02-02 10:16:40 -0800

    code.js: TSPD remove: y axis at least 20
    due to misplaced x axis labels

--- cmp/code.js
+++ cmp/code.js
@@ -556,7 +556,7 @@ function buildChartHtmlFromCmpData(json = null) {
         xLabels: 'year',
         xLabelAngle: 45,
         ykeys: ykey_tmp,
-        ymax: app.selectedViz == 'TSPD' ? 20 : 'auto',
+        ymax: 'auto',
       });
     }
   }
